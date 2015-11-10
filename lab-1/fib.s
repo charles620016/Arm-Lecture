cae4ed76b5b9956a70e1fabd1b6827b64c6897d0
charles620016 <charles620016@gmail.com>
2015-11-10 17:22:46 +0800

    Modify original version to tail-recursive version

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -8,45 +8,34 @@
 	.global fibonacci
 	.type fibonacci, function
 
+@	int fib(int n, int a, int b){
+@	    if (0 == n) return a;
+@	    if (1 == n) return b;
+@   	 return fib(n-1, b, a+b);
+@	}
+
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
-
-	@ R4 = R0 - 0 (update flags)
-	sub r4, r0, #0
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-	cmp r0, #0
-	ble .L3
-
-	@ Compare R4 wtih 1
-	cmp r4, #1
-	@ If R4 == 1 goto .L4 (which returns 1)
-	beq .L4
+	@ r0 = n, r1 = a, r2 = b
+	push {lr}
 	
-	@ R0 = R4 - 1
-	sub r0, r4, #1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-	bl fibonacci	
-
-	@ R5 = R0
-	mov r5, r0
-	@ R0 = R4 - 2
-	sub r0, r4, #2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-	bl fibonacci
-	@ R0 = R5 + R0 (update flags)
-	add r0, r5, r0
-	pop {r3, r4, r5, pc}		@EPILOG
-
+	@ Compare r0 wtih 1
+	@ if(r0 <= 0) goto .L3 (which returns 0)
+	cmp r0, #0
+	ble	.L3
+	
+	sub r0, r0, #1				@ n = n - 1
+	add r3, r1, r2				@ temp = a + b
+	mov r1, r2					@ a = b
+	mov r2, r3					@ b = temp
+	bl	fibonacci				@ Recursive call to fibonacci with R4 - 1 as parameter
+	
+	pop {pc}					@EPILOG
 	@ END CODE MODIFICATION
 .L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	mov r0, r1					@ R0 = 0
+	pop {pc}					@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
